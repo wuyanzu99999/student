37aff6553e74b9bde67945357cfde7d753c3d259
suyawen <3337460160@qq.com>
2021-01-04 15:35:49 +0800

    v-01

--- src/components/NavList/index.js
+++ src/components/NavList/index.js
@@ -1,33 +1,111 @@
 import { Component } from 'react'
 import "./index.css"
 export default class componentName extends Component {
+    constructor(){
+        super();
+        this.state={
+            isActive:false,
+        }
+    }
+    changeCss(ev){
+        console.log(ev.target);
+        console.log( ev.target.lastElementChild)
+        ev.target.lastElementChild.style.transform= "rotateZ(90deg)";           
+    }
+    
     render() {
         return (
             <div className="content-nav">
-                <ul>
+                <ul onClick={(ev)=>this.changeCss(ev)}>
                     <h4>Main Menu</h4>
                     <li>
-                        <span>icon</span>
-                            Dashboard</li>
-                    <li><span>icon</span>Students</li>
-                    <li><span>icon</span>Teachers</li>
-                    <li><span>icon</span>Departments</li>
-                    <li><span>icon</span>Subjects</li>
-                    <h4><span>icon</span>Management</h4>
-                    <li><span>icon</span>Accounts</li>
-                    <li><span>icon</span>Holiday</li>
-                    <li><span>icon</span>Fees</li>
-                    <li><span>icon</span>Exam list</li>
-                    <li><span>icon</span>Events</li>
-                    <li><span>icon</span>Time Table</li>
-                    <li><span>icon</span>Library</li>
+                        <span>图标</span>
+                            Dashboard
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>                      
+                            Students
+                         <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Teachers
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>             
+                        Departments
+                        <input type="button" value=">"/>                  
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Subjects
+                        <input type="button" value=">"/>
+                    </li>
+                    <h4> Management</h4>
+                    <li>
+                        <span>图标</span>
+                        Accounts
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Holiday
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Fees
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Exam list
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Events
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Time Table
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Library
+                        <input type="button" value=">"/>
+                    </li>
                     <h4>Pages</h4>
-                    <li><span>icon</span>Authentication</li>
-                    <li><span>icon</span>Blank Page</li>
+                    <li>
+                        <span>图标</span>
+                        Authentication
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Blank Page
+                        <input type="button" value=">"/>
+                    </li>
                     <h4>Others</h4>
-                    <li><span>icon</span>Sports</li>
-                    <li><span>icon</span>Hostel</li>
-                    <li><span>icon</span>Transport</li>
+                    <li>
+                        <span>图标</span>
+                        Sports
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Hostel
+                        <input type="button" value=">"/>
+                    </li>
+                    <li>
+                        <span>图标</span>
+                        Transport
+                        <input type="button" value=">"/>
+                    </li>
                 </ul>
             </div>
         )
